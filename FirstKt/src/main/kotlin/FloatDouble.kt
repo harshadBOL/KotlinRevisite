b846b80b47d4d57359cdd4cc65a6bb1d4538d6f3
harshadBOL <harshad.pawar@lji.io>
2023-02-15 11:25:42 +0530

    float and double

---
+++ FirstKt/src/main/kotlin/FloatDouble.kt
@@ -0,0 +1,11 @@
+fun main() {
+    /**
+     * float  : Decimal numbers Holds 32 bits
+     * Double : Holds 64 bits
+     * */
+
+    val pi = 3.14f // float value
+    val piDouble = 3.1434562
+    println("The value of Pi in float value $pi")
+    println("The value of Pi in Double value $piDouble")
+}
