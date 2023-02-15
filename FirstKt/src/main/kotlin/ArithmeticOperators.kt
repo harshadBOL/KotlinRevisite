b8992ec5a0170030d89bca0328390f80fa1e4d98
harshadBOL <harshad.pawar@lji.io>
2023-02-15 12:59:45 +0530

    arithmetic operators

---
+++ FirstKt/src/main/kotlin/ArithmeticOperators.kt
@@ -0,0 +1,8 @@
+fun main() {
+   val a = 50
+   val b = 90
+   println("Addition of $a and $b  : ${a+b}")
+   println("Subtraction of $a and $b : ${b-a}")
+   println("Multiplication of $a and $b : ${b*a}")
+   println("Division of $a and $b : ${b/a}")
+}
