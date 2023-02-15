b1ab7ff61450c6911585b15b118a2e7f87416868
harshadBOL <harshad.pawar@lji.io>
2023-02-15 10:33:57 +0530

    kotlin basic var/val

---
+++ FirstKt/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    /**
+     * var = mutable
+     * val = immutable
+     *
+     * */
+
+    var firstName = "Harshad"
+    val lastName = "Pawar"
+        println("Hello $firstName $lastName")
+}
