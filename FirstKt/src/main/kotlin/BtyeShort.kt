cf0df475a75c4f0faac30e8e9e48185eacb6d0e2
harshadBOL <harshad.pawar@lji.io>
2023-02-15 11:07:45 +0530

    byte and short

---
+++ FirstKt/src/main/kotlin/BtyeShort.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    /**
+     * byte range 0 to 127
+     * short range 0 to 12999
+     * */
+
+    val myByte: Byte
+    myByte = 127
+    val myShort: Short
+    myShort = 12999
+    println("byte value is $myByte short value is $myShort")
+}
