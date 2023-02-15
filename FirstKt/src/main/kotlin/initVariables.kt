2e07ef783dceaaa23561f8ba8b63c47ab5223449
harshadBOL <harshad.pawar@lji.io>
2023-02-15 10:49:02 +0530

    val initialization

---
+++ FirstKt/src/main/kotlin/initVariables.kt
@@ -0,0 +1,10 @@
+fun main() {
+    /**
+     * how to initialize variables
+     * */
+    val myName: String
+    val age: Int
+    myName = "Harshad"
+    age = 23
+    println("My Name Is $myName I'm $age")
+}
